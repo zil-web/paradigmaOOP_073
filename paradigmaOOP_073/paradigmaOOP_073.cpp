164dabb19a7b74becb8fa6e458da1b968e8570eb
MuhSaktiR <seken876@gmail.com>
2023-05-24 19:26:05 +0700

    class orang

--- paradigmaOOP_073/paradigmaOOP_073.cpp
+++ paradigmaOOP_073/paradigmaOOP_073.cpp
@@ -1,3 +1,15 @@
 #include<iostream>
 #include<string>
 using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "string dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
