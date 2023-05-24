9033d1cc0b3f83a6fda541bf67993a9c2546ada8
MuhSaktiR <seken876@gmail.com>
2023-05-24 19:39:05 +0700

    public dokter

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -16,3 +16,29 @@ public:
 	void tambahDokter(dokter*);
 	void cetakDokter();
 };
+class dokter {
+public:
+	string nama;
+	vector<pasien*> daftar_pasien;
+
+	dokter(string pNama) :nama(pNama) {
+		cout << "Dokter \"" << nama << "\" ada \n";
+	}
+	~dokter() {
+		cout << "Dokter \"" << nama << "\" tidak ada \n";
+	}
+
+	void tambahPasien(pasien*);
+	void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter* pDokter) {
+	daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter() {
+	cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
