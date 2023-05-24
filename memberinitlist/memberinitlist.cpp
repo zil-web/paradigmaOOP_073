e7b989fca9f6029f32193279b947c5f14c128f54
MuhSaktiR <seken876@gmail.com>
2023-05-24 20:26:53 +0700

    membuat class

--- memberinitlist/memberinitlist.cpp
+++ memberinitlist/memberinitlist.cpp
@@ -8,20 +8,14 @@ private:
 	string nama;
 	float nilai;
 
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //Member Initialization List
+		//Definisi
+	}
+
 	~mahasiswa() {
 		cout << "Id     = " << id << endl;
 		cout << "Nama   = " << nama << endl;
 		cout << "Nilai  = " << nilai << endl;
 	}
 };
-~mahasiswa() {
-	cout << "Id     = " << id << endl;
-	cout << "Nama   = " << nama << endl;
-	cout << "Nilai  = " << nilai << endl;
-}
-};
-int main() {
-	mahasiswa mhs(12, "Asroni", 90.5);
-
-	return 0;
-}
